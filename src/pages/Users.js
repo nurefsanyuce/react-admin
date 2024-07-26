820ca37f53f2e421820a7be3a29c9012132c4c87
nurefsanyuce96 <nurefsanyuce96@gmail.com>
2024-07-26 14:13:05 +0300

    deneme

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -10,7 +10,7 @@ const Users = () => {
           <div className="container-fluid">
             <div className="row mb-2">
               <div className="col-sm-6">
-                <h1 className="m-0">Starter Page</h1>
+                <h1 className="m-0">Users Page</h1>
               </div>
               <div className="col-sm-6">
                 <ol className="breadcrumb float-sm-right">
